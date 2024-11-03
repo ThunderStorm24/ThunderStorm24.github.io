816ff397bfb9d01c4757a24ef992ca99c650126c
Thunder <100785565+ThunderStorm24@users.noreply.github.com>
2024-11-03 19:37:50 +0100

    First changes
    navbar, main section, about section, skills section, profile section (in progress) and contact section (in progress). In plans some animations

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener("scroll", function() {
+    var navbar = document.querySelector("nav");
+    navbar.classList.toggle("stickyy", window.scrollY > 100); // Zamkniecie nawiasu na końcu linii
+});
