b269e23e009909e45acff33dc94aeebb08da6251
Thunder <100785565+ThunderStorm24@users.noreply.github.com>
2024-11-04 19:53:00 +0100

    some code improvements

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     var navbar = document.querySelector("nav");
     navbar.classList.toggle("stickyy", window.scrollY > 100); // Zamkniecie nawiasu na końcu linii
 });
